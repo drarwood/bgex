39593c64d29956ede89740881156792811f125ec
arw201 <arw201@slade.isad.isadroot.ex.ac.uk>
2023-02-23 23:39:33 +0000

    Unecessary screen output removed

--- BGENProcessor.cpp
+++ BGENProcessor.cpp
@@ -142,7 +142,7 @@ void ExtractGenotypeData(string& f, map<uint64_t,string>& b,
             char varid[varid_length];;
             fread(&varid, 1, sizeof(varid), bgen);
             string varid_s = chars2string(varid, varid_length);
-            cout << "variant id: " << varid_s << endl;
+            //cout << "variant id: " << varid_s << endl;
 
             // set up buffer for rsid
             fread(&buff2, 1, sizeof(buff2), bgen);
@@ -150,7 +150,7 @@ void ExtractGenotypeData(string& f, map<uint64_t,string>& b,
             char rsid[rsid_length];
             fread(&rsid, 1, sizeof(rsid), bgen);
             string rsid_s = chars2string(rsid, sizeof(rsid));
-            cout << "rsid:       " << rsid_s << endl;
+            //cout << "rsid:       " << rsid_s << endl;
             // set up buffer for chromosome
             fread(&buff2, 1, sizeof(buff2), bgen);
             uint16_t chr_length = get_uint16(buff2);
@@ -160,47 +160,47 @@ void ExtractGenotypeData(string& f, map<uint64_t,string>& b,
             if (chr_s.substr(0,1) == "0") {
                 chr_s = chr_s.substr(1,1);
             }
-            cout << "chr:        " << chr_s << endl;
+            //cout << "chr:        " << chr_s << endl;
             // position of variant
             fread(&buff4, 1, sizeof(buff4), bgen);
             uint32_t bp = get_uint32(buff4);
-            cout << "bp:         " << bp << endl;
+            //cout << "bp:         " << bp << endl;
             // number of alleles
             fread(&buff2, 1, sizeof(buff2), bgen);
             uint16_t allele_n = get_uint16(buff2);
-            cout << "alleles:    " << allele_n << endl;
+            //cout << "alleles:    " << allele_n << endl;
             // get allele 1
             fread(&buff4, 1, sizeof(buff4), bgen);
             uint32_t a1_length = get_uint32(buff4);
             char a1[a1_length];
             fread(&a1, 1, sizeof(a1), bgen);
             string a1_s = chars2string(a1, sizeof(a1));
-            cout << "A1:         " << a1_s << endl;
+            //cout << "A1:         " << a1_s << endl;
             // get allele 2
             fread(&buff4, 1, sizeof(buff4), bgen);
             uint32_t a2_length = get_uint32(buff4);
             char a2[a2_length];
             fread(&a2, 1, sizeof(a2), bgen);
             string a2_s = chars2string(a2, sizeof(a2));
-            cout << "A2:         " << a2_s << endl;
-            cout << endl;
+            //cout << "A2:         " << a2_s << endl;
+            //cout << endl;
 
 
             // define bgen varid based on chr:pos:a1a2 here
             string bgen_id = chr_s + ":" + to_string(bp) + ":" + a1_s + ":" + a2_s;
-            cout << "bgen id: " << bgen_id << endl;
+            //cout << "bgen id: " << bgen_id << endl;
             // Now we are at the compressed block
             // 1. Total length of the remainder of the block
             fread(&buff4, 1, sizeof(buff4), bgen);
             unsigned long int remaining_block_length = int((unsigned char)(buff4[3]) << 24 | (unsigned char)(buff4[2]) << 16 | (unsigned char)(buff4[1]) << 8 | (unsigned char)(buff4[0]));
-            cout << "remaining_block_length:    " << remaining_block_length << endl;
+            //cout << "remaining_block_length:    " << remaining_block_length << endl;
             // 2. Total length of the prob data block uncompressed
             fread(&buff4, 1, sizeof(buff4), bgen);
             unsigned long int uncompressed_block_length = int((unsigned char)(buff4[3]) << 24 | (unsigned char)(buff4[2]) << 16 | (unsigned char)(buff4[1]) << 8 | (unsigned char)(buff4[0]));
-            cout << "uncompressed_block_length: " << uncompressed_block_length << endl;
+            //cout << "uncompressed_block_length: " << uncompressed_block_length << endl;
             // 3. Size of compressed block
             unsigned long int compressed_block_length = remaining_block_length-4;
-            cout << "compressed_block_length:   " << compressed_block_length << endl;
+            //cout << "compressed_block_length:   " << compressed_block_length << endl;
 
 
             // Get compressed block
@@ -209,24 +209,24 @@ void ExtractGenotypeData(string& f, map<uint64_t,string>& b,
             // decompress
             unsigned char uncompressed_block[uncompressed_block_length];
             uncompress(uncompressed_block, &uncompressed_block_length,  comp_block, sizeof(comp_block));
-            cout << sizeof(uncompressed_block) << endl;
+            //cout << sizeof(uncompressed_block) << endl;
 
             // get first 4 bytes from buffer to confirm N
             for (int i = 0; i < 4; ++i) {
                 buff4[i] = uncompressed_block[i];
             }
             uint32_t varN = get_uint32(buff4);
-            cout << "Number of individuals " << varN << endl;
+            //cout << "Number of individuals " << varN << endl;
             // get number of alleles for this variant
             for (int i = 4; i < 5; ++i) {
                 buff2[i-4] = uncompressed_block[i];
             }
             uint16_t alleleN = get_uint16(buff2);
-            cout << "Number of alleles " << alleleN << endl;
+            //cout << "Number of alleles " << alleleN << endl;
 
             // Get the number of bits representing each prob
             unsigned int bits_per_prob = uncompressed_block[8+varN+1];
-            cout << "Number of bits per probability " << bits_per_prob << endl;
+            //cout << "Number of bits per probability " << bits_per_prob << endl;
 
 
             // cycle through the rest of the buffer getting the probabilites
